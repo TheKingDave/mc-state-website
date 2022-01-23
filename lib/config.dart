a9b4f9ce0096eed8309ef31e8e057eb5b80d36d6
TheKingDave <david@langheiter.com>
2022-01-23 17:35:37 +0100

    Moved from hostname to host, so it works on docker

--- lib/config.dart
+++ lib/config.dart
@@ -3,19 +3,19 @@ import 'package:dotenv/dotenv.dart' show load, env;
 
 class Config {
   late final int port;
-  final String hostname;
+  final String host;
   final String server;
   final String webPath;
 
   Config._(
       {required this.port,
-      required this.hostname,
+      required this.host,
       required this.server,
       required this.webPath});
 
   Config._str(
       {required String port,
-      required this.hostname,
+      required this.host,
       required this.server,
       required this.webPath}) {
     this.port = int.parse(port);
@@ -24,17 +24,20 @@ class Config {
   factory Config(List<String> arguments) {
     final parser = ArgParser();
     parser.addOption('port', abbr: 'p');
-    parser.addOption('hostname', abbr: 'h');
+    parser.addOption('host', abbr: 'h');
     parser.addOption('server', abbr: 's');
     parser.addOption('webPath', abbr: 'w');
 
     final result = parser.parse(arguments);
 
+    print(env);
     load();
+    print(env);
+    
     return Config._str(
       port: result['port'] ?? env['PORT'] ?? '8000',
-      hostname: result['hostname'] ?? env['HOSTNAME'] ?? '0.0.0.0',
-      server: result['server'] ?? env['SERVER'] ?? 'localhost',
+      host: result['host'] ?? env['HOST'] ?? '127.0.0.1',
+      server: result['server'] ?? env['SERVER'] ?? '127.0.0.1',
       webPath: result['webPath'] ?? env['WEB_PATH'] ?? 'web/public',
     );
   }
